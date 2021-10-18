9dfb161605e1a6a8610fd8fbb5a70aadbdbb00ce
Eithan Refaely <eithanrefaely@gmail.com>
2021-10-18 13:43:16 +0300

    Finished TimeFromAToB

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const timeToNow = (dateArr) => {
  return moment(dateArr).fromNow();
     } catch(error) {
         console.error(error);
-        console.log("You Should call the function with an array, example: [2007, 0, 29]")
+        console.log("You Should call the function with an array, example: ([2007, 0, 29])")
+    }
+}
+
+const TimeFromAToB = (firstDateArr, secondDateArr) => {
+    try {
+        return moment(firstDateArr).from(moment(secondDateArr));
+    } catch (error) {
+        console.error(error);
+        console.log("You Should call the function with two arrays, example: ([2007, 0, 28], [2007, 0, 29])")
     }
 }
 
